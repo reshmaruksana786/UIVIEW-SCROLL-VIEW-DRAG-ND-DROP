33fded5df1861254d11f1fbd2ab4c057c04a9ed5
Syed.Reshma Ruksana <reshmasmile468@achiever-8.local>
2019-09-23 12:27:45 +0530

    Initial Commit

---
+++ UIVIEW &SCROLL VIEW1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIVIEW &SCROLL VIEW1
+//
+//  Created by Syed.Reshma Ruksana on 23/09/19.
+//  Copyright © 2019 Syed.Reshma Ruksana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
